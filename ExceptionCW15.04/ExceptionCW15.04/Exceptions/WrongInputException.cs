cec6c8e805cb383119b28de3fbd39942b721c5ed
anargasimzada <tu7g11auv@code.edu.az>
2024-04-15 22:04:01 +0400

    exception

---
+++ ExceptionCW15.04/ExceptionCW15.04/Exceptions/WrongInputException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionCW15._04.Exceptions
+{
+    internal class WrongInputException:Exception
+    {
+        public WrongInputException(string message) :base(message){ }
+    }
+}
